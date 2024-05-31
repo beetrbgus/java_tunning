00913136dece238ccee2265602ffc20fdd97ef08
beetrbgus <devbeetrb0214@gmail.com>
2024-05-31 13:12:14 +0900

    Initial commit

---
+++ src/main/java/com/beetrb/java_tunning/JavaTunningApplication.java
@@ -0,0 +1,13 @@
+package com.beetrb.java_tunning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTunningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTunningApplication.class, args);
+    }
+
+}
